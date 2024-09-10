5378a86a35592afab15b106379166201d55ab65b
krishsh93 <krishff48@gail.com>
2024-09-10 08:40:31 +0530

    signup

--- openshop-analytics-dashboard/src/components/PopularProducts.jsx
+++ openshop-analytics-dashboard/src/components/PopularProducts.jsx
@@ -48,40 +48,49 @@ const popularProducts = [
 ]
 
 function PopularProducts() {
+	const [applications, setApplications] = React.useState([]);
+	useEffect(() => {
+		fetchPatents();
+	  }, []);
+	
+	  const fetchPatents = async () => {
+		try {
+		  const response = await fetch('http://localhost:5000/api/user/patents');
+		  const data = await response.json();
+		  setApplications(data);
+		} catch (error) {
+		  console.error('Error fetching patents:', error);
+		}
+	  };
 	return (
 		<div className="w-[20rem] bg-white p-4 rounded-sm border border-gray-200">
-			<strong className="text-gray-700 font-medium">Popular Products</strong>
+			<strong className="text-gray-700 font-medium">Your Patents</strong>
 			<div className="mt-4 flex flex-col gap-3">
-				{popularProducts.map((product) => (
-					<Link
-						key={product.id}
-						to={`/product/${product.id}`}
-						className="flex items-start hover:no-underline"
-					>
-						<div className="w-10 h-10 min-w-[2.5rem] bg-gray-200 rounded-sm">
+				{applications.map((product) => (
+					<div>
+						{/* <div className="w-10 h-10 min-w-[2.5rem] bg-gray-200 rounded-sm">
 							<img
 								className="w-full h-full object-cover rounded-sm"
 								src={product.product_thumbnail}
 								alt={product.product_name}
 							/>
-						</div>
+						</div> */}
 						<div className="ml-4 flex-1">
-							<p className="text-sm text-gray-800">{product.product_name}</p>
+							<p className="text-sm text-gray-800">{product.title}</p>
 							<span
 								className={classNames(
-									product.product_stock === 0
+									product.status === 'Pending'
 										? 'text-red-500'
-										: product.product_stock > 50
-										? 'text-green-500'
-										: 'text-orange-500',
+										:  'text-green-500',
+										
 									'text-xs font-medium'
 								)}
 							>
-								{product.product_stock === 0 ? 'Out of Stock' : product.product_stock + ' in Stock'}
+								{product.status === 'Pending' ? 'Pending' : product.status }
 							</span>
 						</div>
-						<div className="text-xs text-gray-400 pl-1.5">{product.product_price}</div>
-					</Link>
+						{/* <div className="text-xs text-gray-400 pl-1.5">{product.product_price}</div> */}
+					</div>
 				))}
 			</div>
 		</div>
